6ce875edb72bf5c6eeaf6fa8c38daeda1defef7d
Christian <christiancastilloalexis@gmail.com>
2022-10-03 22:23:12 -0700

    christian

--- src/Users/user/user.service.ts
+++ src/Users/user/user.service.ts
@@ -17,5 +17,38 @@ export class UserService {
         return this.Users.find((user) => user.email === email)
     }
     
+    updateUserbyID( id:number, user:userModel ):boolean{
+        let user_index = this.Users.findIndex( (user) => user.id === id)
+        const olduser = this.Users[user_index]
+
+        if(user_index !== -1){
+            this.Users[user_index] = {
+                id: user.id,
+                name: user.name,
+                email: user.email,
+                cellphone: user.cellphone,
+                
+            }
+
+            this.checkOldUser(olduser,user_index)
+            return true
+        }
+        return false
+    }
+
+    checkOldUser(olduser:userModel,user_index:number){
+        if (this.Users[user_index].id === undefined) {
+            this.Users[user_index].id = olduser.id
+        }
+        if (this.Users[user_index].name === undefined) {
+            this.Users[user_index].name = olduser.name
+        }
+        if (this.Users[user_index].email === undefined) {
+            this.Users[user_index].email = olduser.email
+        }
+        if (this.Users[user_index].cellphone === undefined) {
+            this.Users[user_index].cellphone = olduser.cellphone
+        }
+    }
 }
 
