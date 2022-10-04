68202494e522677a789efde03b23cf67b5aaaa74
Christian <christiancastilloalexis@gmail.com>
2022-10-03 22:02:32 -0700

    christian

--- src/Users/user/user.controller.ts
+++ src/Users/user/user.controller.ts
@@ -3,7 +3,7 @@ import { Body, Get, Controller, Post, Param } from '@nestjs/common';
 import { UserService } from './user.service';
 import { get } from 'http';
 
-@Controller('/user')
+@Controller('user')
 export class UserController {
     constructor (private userService: UserService){
         
@@ -11,16 +11,26 @@ export class UserController {
     @Post()
     create(@Body() params: userModel):void{
         this.userService.create(params)
-        /*console.log("Nombre es: " + params.name, "\nCorreo es: " + params.email,
-        "n\Telefono es: " + params.cellphone)*/
+        /*console.log("Nombre es: " + params.name, "\nCorreo es: " + params.email, "n\Telefono es: " + params.cellphone)*/
 
     }
+
      @Get('/all')
      getUsers(): userModel[] {
         return this.userService.getAll()
      }
-     @Get('/:id')
-     getUser(@Param('email') params) : userModel{
-        return this.userService.getByEmail(params.email)
+     
+     @Get('/:email')
+     getUser(@Param('email') params) : userModel | string{
+        const user = this.userService.getByEmail(params)
+        return user ?? "El usuario no existe"
      }
 }
+//crear nueva rama -> git checkout -b *nombre de la rama*
+//git add . && git commit -m *comentario*
+// git commit -am *comentario* UNICAMENTE FUNCIONA CON ARCHIVOS QUE FUERON MODIFICADOS,
+//NO AGREGADOS O ELIMINADOS
+//Git push origin ValidarUsuario NO SE MANDA A MAIN
+//git checkout main
+//git pull origin main
+//git status *te dice el status de tu main, si está desactualizado y cuantos commits atrás
